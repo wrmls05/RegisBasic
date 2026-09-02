33a8b01beabe6f41cb5f6970746e8d797f5b8af1
Owen <woramet.le@ku.th>
2026-09-02 14:40:56 +0700

    Add Field component with basic structure and imports

---
+++ src/components/Field.js
@@ -0,0 +1,13 @@
+import { View, Text, TextInput } from "react-native";
+import { styles } from "../styles/registerStyles";
+import { colors } from "../styles/theme";
+
+const Field = () => {
+    return(
+        <View>
+            <Text>Field</Text>
+        </View>
+    )
+}
+
+export default Field;
